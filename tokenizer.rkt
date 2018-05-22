9467cd8f5d9b1f5d53fc23af7652826e06b7964e
Lukas Lazarek <LukasALazarek@gmail.com>
2018-05-22 16:41:03 -0400

    Made single-line sexps remove their line from output

--- tokenizer.rkt
+++ tokenizer.rkt
@@ -7,9 +7,15 @@
     (define ptext-lexer
       (lexer
        [(eof) eof]
-       [(from/to "!@" "@!")
+       [(from/to "\n!@" "@!\n")
         ;; Read the sexp so that SEXP-TOK contains a sexp rather than
         ;; a string; this lets us decompose it in the expander.
+        ;; This is the only thing on this line, so remove the line from output
+        (token 'SEXP-TOK (read (open-input-string
+                                (trim-ends "\n!@" lexeme "@!\n"))))]
+       [(from/to "!@" "@!")
+        ;; "Embedded" expressions (not sole thing on line) leave
+        ;; surroundings completely intact
         (token 'SEXP-TOK (read (open-input-string
                                 (trim-ends "!@" lexeme "@!"))))]
        [any-char (token 'CHAR-TOK lexeme)]))
